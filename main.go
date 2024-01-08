a337e66b7327fb5be6947b74584602b36bfcde26
ootashiori <info@ota-grimes.com>
2024-01-08 19:13:44 +0900

    first

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"go-rest-api/controller"
+	"go-rest-api/db"
+	"go-rest-api/repository"
+	"go-rest-api/router"
+	"go-rest-api/usecase"
+)
+
+func main() {
+	db := db.NewDB()
+	userRepository := repository.NewUserRepository(db)
+	taskRepository := repository.NewTaskRepository(db)
+	userUsecase := usecase.NewUserUsecase(userRepository)
+	taskUsecase := usecase.NewTaskUsecase(taskRepository)
+	userController := controller.NewUserController((userUsecase))
+	taskController := controller.NewTaskController((taskUsecase))
+	e := router.NewRouter(userController, taskController)
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
